7ee331a0c9b6d7329f9c5797ee4948e8721086b3
PCassemiro <pedrobr2909@gmail.com>
2024-09-17 16:58:39 -0300

    inicio de mini projeto

---
+++ script.js
@@ -0,0 +1,7 @@
+var numero1=window.document.getElementById('n1')
+var numero2=window.document.getElementById('n2')
+var res=window.document.getElementById('Resposta')
+var resposta= res.value
+function clicar() {
+    =`${numero1} + ${numero2}`
+}
